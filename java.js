10d583b789566f8f644d98e14ca4a07bb84559d0
Your Name <benja902674@gmail.com>
2023-08-05 03:00:45 -0500

    firts commit

---
+++ java.js
@@ -0,0 +1,39 @@
+// var swiper = new Swiper('.mySwiper', {
+//     effect: 'coverflow',
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: 'auto',
+//     coverflowEffect: {
+//       rotate: 15,
+//       stretch: 0,
+//       depth: 300,
+//       modifier: 1,
+//       slideShaows: true,
+//     },
+//     loop: true,
+//   });
+
+  var swiper = new Swiper('.mySwiper', {
+    autoplay: {
+      delay: 2000
+    },
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
